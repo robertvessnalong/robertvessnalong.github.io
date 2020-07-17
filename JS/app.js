0cc338a692a6c7719db0e02c750168a7aa1c6fea
Robert Long <pongrobertlong@gmail.com>
2020-07-17 09:34:21 -0400

    -Added JS and Color to Button
    - JavaScript Added for a links when clicked
    - Changed Color of "View My Projects"

--- JS/app.js
+++ JS/app.js
@@ -21,6 +21,12 @@ hamburgerMenu.addEventListener("click", () => {
     } else {
       link.style.animation = `menuitems 0.5s ease forwards ${index / 5 + 0.5}s`;
     }
+    //This will close the menu when an a link is pressed
+    link.addEventListener("click", () => {
+      navMenu.classList.remove("open"); //Will Remove the open class from Nav Menu
+      hamburgerMenu.classList.remove("toggle"); //Will remove toggle class from Hamburger Menu
+      navLinks.forEach((link) => (link.style.animation = "")); // For each link will remove animatons so they can be added again
+    });
   });
   //Rotate Hamburger to 'X'
   hamburgerMenu.classList.toggle("toggle");
